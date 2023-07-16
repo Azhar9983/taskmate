8a39597d2795d5d8ee9923f775c9f10bbe982d88
Azharuddin Ansari <azharuddinansari232@gmail.com>
2023-07-17 01:50:46 +0530

    Implemented CRUD functionality in TaskMate

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -15,6 +15,35 @@ def todolist(request):
     else:
         all_tasks = TodoList.objects.all
         return render(request, 'todolist.html', { 'all_tasks': all_tasks } )
+    
+def delete_task(request, task_id):
+    task = TodoList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
+
+def edit_task(request, task_id):
+    if request.method == 'POST':
+        task = TodoList.objects.get(pk=task_id)
+        form  = TodoForm(request.POST or None, instance= task)
+        if form.is_valid():
+            form.save()
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+    else:
+        task_obj = TodoList.objects.get(pk=task_id)
+        return render(request, 'edit.html', { 'task_obj': task_obj } )
+    
+def complete_task(request, task_id):
+    task = TodoList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+def pending_task(request, task_id):
+    task = TodoList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
 
 def contact(request):
     context = {
